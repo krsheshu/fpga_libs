fba926eb538b0caabf7a0a77907a5865d421e94a
Sheshu Kalaparmbathu Ramanandan <Sheshu.Ramanandan@smartray.com>
2020-05-23 13:41:14 +0200

    libs: corrected sytax error with single bit cdc module

--- hdl/libs/cdc/singleBitCdc/verilog/singleBitCdc.v
+++ hdl/libs/cdc/singleBitCdc/verilog/singleBitCdc.v
@@ -3,19 +3,20 @@
 * Assuming frequency of clkB > clkA for no data loss
 */
 
-module singleBitCdc (
-                        input wire     clkA           ;
-                        input wire     bitA           ;
-                        input wire     clkB           ;
-                        output wire    sync_bitB
-                    );
+module singleBitCdc (   input wire     clkA           ,
+                        input wire     bitA           ,
+                        input wire     clkB           ,
+                        output wire    sync_bitB      );
 
 
   reg     reg_bitA;
+  reg     reg_sync_bitB;
+
+  assign sync_bitB    = reg_sync_bitB;
 
   always  @(posedge clkB) begin
 
-    {sync_bitB, reg_bitA}   <= {reg_bitA, bitA};
+    {reg_sync_bitB, reg_bitA}   <= {reg_bitA, bitA};
 
   end
 
