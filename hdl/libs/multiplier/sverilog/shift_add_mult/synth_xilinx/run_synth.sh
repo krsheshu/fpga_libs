aaad448c3da04bc294eb49e46a6cc846be8f3dd4
Sheshu Kalaparmbathu Ramanandan <Sheshu.Ramanandan@smartray.com>
2020-05-24 17:41:28 +0200

    multiplier: names modified

--- hdl/libs/multiplier/sverilog/shift_add_mult/synth_xilinx/run_synth.sh
+++ hdl/libs/multiplier/sverilog/shift_add_mult/synth_xilinx/run_synth.sh
@@ -2,8 +2,8 @@
 
 export TOP="shift_add_mult"
 
-vivado -mode batch -source scr/proj_flow/v192_cheetah_v1_proj.tcl -notrace
-#vivado -mode batch -source scr/non_proj_flow/v192_cheetah_v1_proj.tcl -notrace
+time vivado -mode batch -source scr/proj_flow/v192_mult_proj.tcl -notrace
+#time vivado -mode batch -source scr/non_proj_flow/v192_mult_proj.tcl -notrace
 
 # Removing backup journal and log files
 rm *backup.jou *backup.log
